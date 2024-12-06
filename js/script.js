eead6bbd3f33ac750aa2ac92531d8f64846b7f4d
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-12-06 14:39:15 +0600

    faq started

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+//for navbar toggle
 const nav = document.querySelector('.navbar');
 const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener("click",() => {
     nav.classList.toggle('active')
-})
+});
+
+//for FAQ toggle
+const question = document.querySelector('.question');
+const questionTitle = document.querySelector('.question-title');
